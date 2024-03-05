6de25c9ee726d7f949519e4386c9c9887ff66d7f
Daniel <daniel.msouza@hotmail.com>
2024-03-05 11:28:45 -0300

    ex

--- ex04.py
+++ ex04.py
@@ -1,9 +1,8 @@
 '''Create a class called Restaurant with the attributes name, category, asset and create 2 more attributes.'''
 
 class Restaurant:
-    def __init__(self, name, category, asset=False, capacity, grade):
+    def __init__(self, name, category, asset=False):
         self.name = name
         self.category = category
         self.asset = asset
-        self.capacity = capacity
         
